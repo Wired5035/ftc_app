ffe968e9a7187d9886910189d109c63309af50c0
Wired5035 <WiredFTC@gmail.com>
2015-12-15 19:43:41 -0600

    cleaned up registery class

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -55,22 +55,23 @@ public class FtcOpModeRegister implements OpModeRegister {
      * If two or more op modes are registered with the same name, the app will display an error.
      */
 
-    manager.register("NullOp", NullOp.class);
+    //manager.register("NullOp", NullOp.class);
 
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register("K9Line", K9Line.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    //manager.register("K9TeleOp", K9TeleOp.class);
+    //manager.register("K9Line", K9Line.class);
+    //manager.register ("PushBotAuto", PushBotAuto.class);
+    //manager.register ("PushBotManual", PushBotManual.class);
     manager.register ("HangBotPro", HangingRobotTeleOp.class);
     manager.register("TestPosition", testServoPosition.class);
-    manager.register("Team10618", Team10618.class);
+    manager.register("AutoDropOff",AutoDropOff.class);
+    //manager.register("Team10618", Team10618.class);
 
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
-    manager.register("MR Gyro Test", MRGyroTest.class);
+    //manager.register("MR Gyro Test", MRGyroTest.class);
 
     //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
     //manager.register("ColorSensorDriver", ColorSensorDriver.class);
@@ -85,9 +86,9 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register("LinearK9TeleOp", LinearK9TeleOp.class);
     //manager.register("LinearIrExample", LinearIrExample.class);
 
-    manager.register("ProBotTeleOP", ProBotTeleOP.class);
-    manager.register("OPtestdeadzone", OPtestdeadzone.class);
-    manager.register("DetectColor", DetectColor.class);
+    //manager.register("ProBotTeleOP", ProBotTeleOP.class);
+    //manager.register("OPtestdeadzone", OPtestdeadzone.class);
+    //manager.register("DetectColor", DetectColor.class);
 
    
     //manager.register ("PushBotManual1", PushBotManual1.class);
