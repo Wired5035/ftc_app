0a5ca4015a6318f7b70ca7402e6cc77addad0e2d
Wired5035 <WiredFTC@gmail.com>
2015-12-15 19:44:55 -0600

    new OpMode our first Autonomous

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoDropOff.java
@@ -0,0 +1,35 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import android.os.CountDownTimer;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Kota Baer on 12/15/2015.
+ */
+public class AutoDropOff extends LinearOpMode {
+
+    //Variables section:
+    int timeLeft = 10;
+    ElapsedTime CountingUp = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        CountingUp.reset();
+        telemetry.addData("Status: ", "WAITING.....");
+        waitForStart();
+        telemetry.addData("CountUp", ((float) CountingUp.time()));
+        sleep(2000);
+        CountingUp.reset();
+
+        while (true) {
+            telemetry.addData("Status: ", "STARTING.....");
+            telemetry.addData("CountUp", ((int) CountingUp.time()));
+            telemetry.addData("TimeLeft: ", timeLeft);
+            sleep(1000);
+        }
+    }
+}
