c285ba033e021d25cb2866643d3da803e0e258b9
Kota <WiredFTC@gmail.com>
2017-10-24 18:59:53 -0500

    programs from onbotjava

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedClose2K17.java
@@ -0,0 +1,194 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This OpMode illustrates the basics of using the Vuforia engine to determine
+ * the identity of Vuforia VuMarks encountered on the field. The code is structured as
+ * a LinearOpMode. It shares much structure with {@link ConceptVuforiaNavigation}; we do not here
+ * duplicate the core Vuforia documentation found there, but rather instead focus on the
+ * differences between the use of Vuforia for navigation vs VuMark identification.
+ *
+ * @see ConceptVuforiaNavigation
+ * @see VuforiaLocalizer
+ * @see VuforiaTrackableDefaultListener
+ * see  ftc_app/doc/tutorial/FTC_FieldCoordinateSystemDefinition.pdf
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
+ *
+ * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
+ * is explained in {@link ConceptVuforiaNavigation}.
+ */
+
+@Autonomous(name="Concept: VuMark Id", group ="Concept")
+
+public class AutoRedClose2K17 extends LinearOpMode {
+    Hardware2k17 robot = new Hardware2k17();
+    private ElapsedTime runtime = new ElapsedTime();
+    
+
+    public static final String TAG = "Vuforia VuMark Sample";
+
+    OpenGLMatrix lastLocation = null;
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    VuforiaLocalizer vuforia;
+
+    @Override public void runOpMode() {
+        robot.init(hardwareMap, this);
+
+        /*
+         * To start up Vuforia, tell it the view that we wish to use for camera monitor (on the RC phone);
+         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
+         */
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        // OR...  Do Not Activate the Camera Monitor View, to save power
+        // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        /*
+         * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+         * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+         * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+         * web site at https://developer.vuforia.com/license-manager.
+         *
+         * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+         * random data. As an example, here is a example of a fragment of a valid key:
+         *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+         * Once you've obtained a license key, copy the string from the Vuforia web site
+         * and paste it in to your code onthe next line, between the double quotes.
+         */
+        parameters.vuforiaLicenseKey = "AfXb48r/////AAAAGeD2f/Vqr091sIpDI7RLaYYXDM4Ao03klW8aOZpnKhW7owlW94atv0FpmrIMSu8f15XxGzIXZa9xjWrEw+Cqnea2mZE/FuHbD6WUGnU1Mwyy8CzejVRQV0dTu2Y/KuS9nxcCMcMDKnH3OZjFZYJLPgJ3TqqgL47MkEszN/iS8LKg82rPhB81mh3t5c7ZohzPRNDhvrgUOQHruNu+7YcjilNMbtqBGutFkNxJ5qSbA1WajcXwIrgMwvQFDMnr3O1kqo5Mks4lYReyNvczQ4I7TZuRtqox4SzZf9hJN7EfuOGVwRX8YdOTyMMOnekK7lJSNbdydaTQA3ye0eLxO90kOX1zOhexEzGO9WPFiG3hN/s4";
+
+        /*
+         * We also indicate which camera on the RC that we wish to use.
+         * Here we chose the back (HiRes) camera (for greater range), but
+         * for a competition robot, the front camera might be more convenient.
+         */
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        /**
+         * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
+         * in this data set: all three of the VuMarks in the game were created from this one template,
+         * but differ in their instance id information.
+         * @see VuMarkInstanceId
+         */
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
+        telemetry.addData(">", "Press Play to start");
+        telemetry.update();
+        waitForStart();
+
+        relicTrackables.activate();
+        RelicRecoveryVuMark vuMark;
+
+        // #step 1 "scan pictograph to get key column"
+        while (opModeIsActive()) {
+
+            /**
+             * See if any of the instances of {@link relicTemplate} are currently visible.
+             * {@link RelicRecoveryVuMark} is an enum which can have the following values:
+             * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
+             * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
+             */
+            vuMark = RelicRecoveryVuMark.from(relicTemplate);
+            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+
+                /* Found an instance of the template. In the actual game, you will probably
+                 * loop until this condition occurs, then move on to act accordingly depending
+                 * on which VuMark was visible. */
+                telemetry.addData("VuMark", "%s visible", vuMark);
+                telemetry.update();
+                break;
+            }
+        }
+        // #end of step 1
+        // #step 2  "drive right with time based drive"
+            robot.driveRight(.5);
+            runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.0)) {
+        }
+        robot.driveStop();
+        // #end of step 2
+        sleep(500);  // wait 1/2 second
+        // #step 3  "drive while scanning for criptobox walls in order to 
+        //            determine the dastance needed to drive the given distance to each cryptobox"
+        robot.driveForward(.5);
+            runtime.reset();
+        while (opModeIsActive()) {        /// learn how to calculate distance between cypherbox walls
+        }
+        // #end of step 3
+        sleep(500);  // wait 1/2 second
+        // #step 4  "using the time to distance result in step 3 drive backwards then drive forwards to the key column"
+        robot.driveBackward(.5);
+            runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.0/* run back the time we ran forward in step 3*/)) {
+        }
+        robot.driveStop();
+        sleep(500);  // wait 1/2 second before driving forward
+        robot.driveForward(.5);
+            runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.0/* run forward to keycypher box using the value we descovered in step 3*/)) {
+        }
+        robot.driveStop();
+        // #end of step 4
+        // #step 5 "dump block"
+            /*run servo arm up to dump block then retract dumping arm*/
+        // #end step 5
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware2k17.java
@@ -0,0 +1,134 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * Created by Kota Baer on 10/11/2016.
+ */
+
+/**
+ * This is NOT an opmode.
+ * <p/>
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ * <p/>
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ * <p/>
+ * Motor channel:  Left  driveReverse motor:        "left_drive"
+ * Motor channel:  Right driveReverse motor:        "right_drive"
+ */
+@Disabled
+public class Hardware2k17 {
+    /* Public OpMode members. */
+    public DcMotor leftMotorFront = null;
+    public DcMotor leftMotorBack = null;
+    public DcMotor rightMotorFront = null;
+    public DcMotor rightMotorBack = null;
+    public Servo armdrive;
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+    LinearOpMode opmode;
+    private ElapsedTime period = new ElapsedTime();
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap, LinearOpMode aOpMode) {
+            // Save reference to Hardware map
+        hwMap = ahwMap;
+        opmode = aOpMode;
+        rightMotorFront = hwMap.dcMotor.get("rf");
+        rightMotorBack = hwMap.dcMotor.get("rb");
+        leftMotorFront = hwMap.dcMotor.get("lf");
+        leftMotorBack = hwMap.dcMotor.get("lb");
+        armdrive = hwMap.servo.get("armdrive");
+
+
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftMotorFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftMotorBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotorBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotorFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        // Set all motors to zero power
+    }
+
+    public void driveRight(double speed) {
+            leftMotorFront.setPower(speed);
+            leftMotorBack.setPower(speed);
+            rightMotorFront.setPower(-speed);
+            rightMotorBack.setPower(-speed);
+    }
+    public void driveLeft(double speed) {
+            leftMotorFront.setPower(-speed);
+            leftMotorBack.setPower(-speed);
+            rightMotorFront.setPower(speed);
+            rightMotorBack.setPower(speed);
+    }
+    public void driveForward(double speed) {
+            leftMotorFront.setPower(speed);
+            leftMotorBack.setPower(-speed);
+            rightMotorFront.setPower(speed);
+            rightMotorBack.setPower(-speed);
+    }
+    public void driveBackward(double speed) {
+            leftMotorFront.setPower(-speed);
+            leftMotorBack.setPower(speed);
+            rightMotorFront.setPower(-speed);
+            rightMotorBack.setPower(speed);
+    }
+    public void driveStop() {
+            leftMotorFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            leftMotorBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rightMotorFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rightMotorBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            leftMotorFront.setPower(0);
+            leftMotorBack.setPower(0);
+            rightMotorFront.setPower(0);
+            rightMotorBack.setPower(0);
+    }
+    public void stop() {
+    }
+
+    /***
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs Length of wait cycle in mSec.
+     * @throws InterruptedException
+     */
+    public void waitForTick(long periodMs) throws InterruptedException {
+
+        long remaining = periodMs - (long) period.milliseconds();
+
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0)
+            Thread.sleep(remaining);
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+
+
+
+}
+
